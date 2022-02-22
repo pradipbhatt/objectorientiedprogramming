1c063a7cab5e7cdf803f5ac5e5a85f08ecd0149b
pradip bhatt <84025490+pradipbhatt@users.noreply.github.com>
2022-02-22 22:21:08 +0545

    Update Static_function.cpp

--- Static_function.cpp
+++ Static_function.cpp
@@ -16,7 +16,7 @@ public:
 
     void showcode() // showcode function is defined
     {
-        cout << "Student code" << code << "/BCTE/077." << endl;
+        cout << "Student code \t " << code << "/BCTE/077." << endl;
     }
 
     static void showcount()
@@ -26,22 +26,19 @@ public:
 };
 int BCTE::count;
 
-
 int main()
 {
-    BCTE c1, c2;
+    BCTE c1, c2, c3; // C1 and C2 objects are created of class BCTE
     system("cls");
-    c1.setcode();
-    c2.setcode();
-
-    BCTE::showcount();
-
-    BCTE c3;
+    c1.setcode(); // c1.called the function setcode.
+    c2.setcode(); // c2.called the function setcode.
     c3.setcode();
 
-    BCTE::showcount();
+    BCTE::showcount(); // static function showcount is called.
 
-    c1.showcode();
-    c2.showcode();
-    c3.showcode();
+
+    c1.showcode(); // object c1 call showcode function.
+    c2.showcode(); // object c2 call showcode function.
+    c3.showcode(); // object c3 call showcode function.
 }
+
