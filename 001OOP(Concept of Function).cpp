16804b31def01cfa0f90aab7d7323eeb39a2b5e9
pradip bhatt <84025490+pradipbhatt@users.noreply.github.com>
2022-02-25 00:32:56 +0545

    Create 001OOP(Concept of Function).cpp

---
+++ 001OOP(Concept of Function).cpp
@@ -0,0 +1,22 @@
+// Create a function
+#include <iostream>
+using namespace std;
+
+void myFunction()
+{
+    cout << "I just got executed!"<<endl;
+}
+
+void display()
+{
+    cout << "My name is pradip bhatt";
+}
+int main()
+{
+    system("cls");
+    myFunction(); // call the function
+    display();
+    return 0;
+}
+
+// Outputs "I just got executed!"
